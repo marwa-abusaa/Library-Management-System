7306db0e19f4968cc72f7c9546951be7d55c7e48
marwa-abusaa <s12028718@stu.najah.edu>
2025-04-23 13:46:24 +0300

    Add query to retrieve all books borrowed by a specific borrower, including unreturned ones

---
+++ Queries/1. List_of_Borrowed_Books.sql
@@ -0,0 +1,14 @@
+declare @BorrowerID int =1;
+
+select 
+	b.BookId, 
+    b.Title, 
+    b.Author, 
+    b.ISBN, 
+    b.Published_Date, 
+    b.Genre, 
+    b.Shelf_Location,
+	l.Date_Borrowed
+from Books b
+join loans l on b.BookID=l.BookID
+where l.BookID=@BorrowerID;
