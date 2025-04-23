6dd777415e2898ed10ff107fa71cdcac4624d87b
marwa-abusaa <s12028718@stu.najah.edu>
2025-04-23 22:34:50 +0300

    Add query: Rank authors by the borrowing frequency of their books.

---
+++ Queries/9.Author_Popularity.sql
@@ -0,0 +1,9 @@
+WITH AuthorBorrowingFrequency AS (
+select b.Author, Count(*) AS [Book Borrowing Frequency]
+from Loans l 
+INNER JOIN Books b on l.BookID = b.BookID
+group by b.Author
+)
+
+select a.*, DENSE_RANK() OVER(ORDER BY a.[Book Borrowing Frequency] DESC) AS AuthorRank
+from AuthorBorrowingFrequency a;
