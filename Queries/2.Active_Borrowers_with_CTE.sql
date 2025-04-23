1c0df72f92e8580789368528fd6b17703e74333c
marwa-abusaa <s12028718@stu.najah.edu>
2025-04-23 22:11:34 +0300

    Add query to find active borrowers with 2+ books and no returns

---
+++ Queries/2.Active_Borrowers_with_CTE.sql
@@ -0,0 +1,9 @@
+WITH ActiveBorrowersWithoutReturns AS (
+	select b.BorrowerID, b.First_Name, b.Last_Name, COUNT(*) AS BorrowerdBooks
+	from Borrowers b
+	join loans l 
+	on b.BorrowerID = l.BorrowerID
+	where l.Date_Returned IS NULL 
+	group by b.BorrowerID, b.First_Name, b.Last_Name
+)
+select * from ActiveBorrowersWithoutReturns WHERE BorrowerdBooks >= 2;
